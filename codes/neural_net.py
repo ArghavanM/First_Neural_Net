887263809f821dc5365b5adbd11ace19a936d42f
ArghavanM <31393841+mod9@users.noreply.github.com>
2017-12-01 10:18:13 -0500

    Added folders to keep codes.

---
+++ codes/neural_net.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class neural_network:
+	def init_with_zeros(self, size):
+	def init_with_rands(self,size):
+	def init_better(self,size):
+	def propagate(self):
+	def SGD_optimization(self):
+	def predict(self):
+
+
